c3e6a07021c9bc61b92620fc78da367ebcbf67a0
luuh-oliveira <lu.oliveira0505@gmail.com>
2021-12-02 08:08:23 -0300

    .

---
+++ cliente/produtos.php
@@ -0,0 +1,19 @@
+<?php
+
+include("../design/header/header.php");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../design/header/header.css">
+    <title>Produtos</title>
+</head>
+<body>
+    <p>Ordenar por:</p>
+</body>
+</html>
--- design/footer/footer.php
+++ design/footer/footer.php
@@ -1,21 +1,18 @@
-<?php
-
-include("../header/header.php");
-?>
-
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="footer.css">
-
+    <link rel="stylesheet" href="../header/header.css">
+    <link rel="stylesheet" href="./footer.css">
     <title>footer</title>
 </head>
 <body>
-    
+    <?php
+    include("../header/header.php")
+    ?>
+
     <div id="footer">
         <div id="contatos">
             <table>
