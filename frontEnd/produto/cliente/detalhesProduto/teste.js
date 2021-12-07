dd1aa4aa0190e36819b036a88cfef5be1b6bab4f
IsaMenegOchi <isabelle.ochini@gmail.com>
2021-12-07 10:07:41 -0300

    Adicionando coisas a tela de detalhes do produto

--- frontEnd/produto/cliente/detalhesProduto/teste.js
+++ frontEnd/produto/cliente/detalhesProduto/teste.js
@@ -1,15 +1,16 @@
+const imgFavorito = document.getElementById("imagemFavoritar");
+const textoFav = document.getElementById("spanFav")
 
-const criarHeader = () => {
+function trocarImagem (){
     
-    const novaDivSlide = document.createElement("div")
-    novaDivSlide.id = `${limparId(urlImage)}`
-    novaDivSlide.classList.add("slide")
-    
-    novaDivSlide.innerHTML = `
-    <div class="imagem-container">
-        <a href="" class="fechar">&#10006;</a>
-        <a href="#zenitsu-agatsuma" class="navegacao anterior">&#171;</a>
-        <img src="${urlImage}" alt="">
-        <a href="#giyu-tomioka" class="navegacao proximo">&#187;</a>
-    </div>`
+    if(imgFavorito.src.includes("linha")){
+        imgFavorito.src = "../../../icons/favorito_preenchido.svg"
+        textoFav.textContent = "Favorito"
+    }
+    else{
+        imgFavorito.src = "../../../icons/favorito_linha.svg"
+        textoFav.textContent = "Favoritar"
+    }
 }
+
+imgFavorito.addEventListener("click", trocarImagem)
